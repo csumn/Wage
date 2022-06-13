cd11f1865a3359f0816aeef9a137762cc04d5df5
csumn <csumn13@gmail.com>
2022-06-13 09:27:52 +0530

    Emp attendance check program added

--- src/com/bridgelabz/EmployeeWageProgram.java
+++ src/com/bridgelabz/EmployeeWageProgram.java
@@ -3,5 +3,12 @@ package com.bridgelabz;
 public class EmployeeWageProgram {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation ");
+		
+		int present = 1;
+		int value = (int)(Math.random()*10)%2;
+		if(value == present) {
+			System.out.println("Emp is present ");
+		}else
+			System.out.println("Emp is Absent");
 	}
 }
