728ed14dac7bf0f33a1a35afd4a151420a6798b7
csumn <csumn13@gmail.com>
2022-06-13 10:06:01 +0530

    Half Day Wage calculate code added

--- src/com/bridgelabz/EmployeeWageProgram.java
+++ src/com/bridgelabz/EmployeeWageProgram.java
@@ -3,14 +3,20 @@ package com.bridgelabz;
 public class EmployeeWageProgram {
 	static final int WAGEPERHOUR = 20;
 	static final int FULLDAYHOURS = 8;
+	static final int HALFDAYHOURS = 4;
+	static final int HALFDAYPRESENT = 2;
+	static final int FULLDAYPRESENT = 1;
 
 	public static void main(String[] args) {
 
-		int present = 1;
-		int value = (int)(Math.random()*10)%2;
-		if(value == present) {
-			System.out.println("Emp is present ");
+		int value = (int)(Math.random()*10)%3;
+		if(value == FULLDAYPRESENT) {
+			System.out.println("Emp is present fullday ");
 			int employeeWage = WAGEPERHOUR *FULLDAYHOURS;
+			System.out.println("Employee Wage is : "+employeeWage);
+		}else if(value == HALFDAYPRESENT) {
+			System.out.println("Emp is present halfday ");
+			int employeeWage = WAGEPERHOUR *HALFDAYHOURS;
 			System.out.println("Employee Wage is : "+employeeWage);
 		}else
 			System.out.println("Emp is Absent : No Wage ");
