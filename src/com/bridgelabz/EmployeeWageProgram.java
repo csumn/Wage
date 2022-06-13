5be59c91fec8c8f56b8ca116c66dbe707f164a0e
csumn <csumn13@gmail.com>
2022-06-13 09:41:05 +0530

    full Day Wage calculated

--- src/com/bridgelabz/EmployeeWageProgram.java
+++ src/com/bridgelabz/EmployeeWageProgram.java
@@ -1,14 +1,18 @@
 package com.bridgelabz;
 
 public class EmployeeWageProgram {
+	static final int WAGEPERHOUR = 20;
+	static final int FULLDAYHOURS = 8;
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation ");
-		
+
 		int present = 1;
 		int value = (int)(Math.random()*10)%2;
 		if(value == present) {
 			System.out.println("Emp is present ");
+			int employeeWage = WAGEPERHOUR *FULLDAYHOURS;
+			System.out.println("Employee Wage is : "+employeeWage);
 		}else
-			System.out.println("Emp is Absent");
+			System.out.println("Emp is Absent : No Wage ");
 	}
 }
